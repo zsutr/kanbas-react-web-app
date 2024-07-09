6d613e51d9bba089bbadd0172c0010f3af524235
zsutr <zsuter21@gmail.com>
2024-07-08 23:04:08 -0400

    Adding repo and name as specified in assignment

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,6 +1,7 @@
 export default function TOC() {
     return (
-      <ul>
+      <ul>    
+        <li><a id="wd-github" href="https://github.com/zsutr/kanbas-react-web-app">Github</a></li>
         <li><a id="wd-a"  href="#/Labs">Labs</a></li>
         <li><a id="wd-a1" href="#/Labs/Lab1">Lab 1</a></li>
         <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      Lindsey Sutermeister
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
