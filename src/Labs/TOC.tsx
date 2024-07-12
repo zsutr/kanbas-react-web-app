543a525c7a6d45e1f1f4a967c033b38a9fde8a12
zsutr <zsuter21@gmail.com>
2024-07-12 19:13:30 -0400

    Revert "Adding repo and name as specified in assignment"
    This reverts commit 6d613e51d9bba089bbadd0172c0010f3af524235.

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,7 +1,6 @@
 export default function TOC() {
     return (
-      <ul>    
-        <li><a id="wd-github" href="https://github.com/zsutr/kanbas-react-web-app">Github</a></li>
+      <ul>
         <li><a id="wd-a"  href="#/Labs">Labs</a></li>
         <li><a id="wd-a1" href="#/Labs/Lab1">Lab 1</a></li>
         <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
